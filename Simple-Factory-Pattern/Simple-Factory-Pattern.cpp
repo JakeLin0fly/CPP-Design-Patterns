567ea4a79169fdb28a384784504184909b404574
JakeLin <jakelin0fly@gmail.com>
2021-11-03 21:43:36 +0800

    简单工厂模式

---
+++ Simple-Factory-Pattern/Simple-Factory-Pattern.cpp
@@ -0,0 +1,47 @@
+//
+// Created by JakeLin on 2021/11/3.
+//
+#include <iostream>
+
+using namespace std;
+
+/**
+ * 抽象产品
+ */
+class Shape {
+public:
+    virtual void draw() = 0;
+};
+
+/**
+ * 具体产品：圆形
+ */
+class Circle : public Shape {
+public:
+    void draw() {
+        cout << "draw a circle" << endl;
+    }
+};
+/**
+ * 具体产品：三角形
+ */
+class Rectangle : public Shape {
+public:
+    void draw() {
+        cout << "draw a rectangle" << endl;
+    }
+};
+
+/**
+ * 工厂：负责实现创建所有实例的内部逻辑
+ */
+class ShapeFactory {
+public:
+    Shape * createShape(string name) {
+        switch (name) {
+            case "circle": return new Circle();
+            case "rectangle": return Rectangle();
+        }
+        return nullptr;
+    }
+};
